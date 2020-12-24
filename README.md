533d69815acf7ec0855fba427c025478a275798d
zhengp0 <zhengp@uw.edu>
2020-12-24 09:34:40 -0800

    initial file structure

--- README.md
+++
@@ -1,2 +0,0 @@
-# ssmod
-State-space model
---
+++ src/ssmod/__about__.py
@@ -0,0 +1,16 @@
+__all__ = [
+    "__title__", "__summary__", "__uri__", "__version__", "__author__",
+    "__email__", "__license__", "__copyright__",
+]
+
+__title__ = "ssmod"
+__summary__ = "State-space model."
+__uri__ = "https://github.com/perceptron-origin/ssmod"
+
+__version__ = "0.0.0"
+
+__author__ = "Perceptron"
+__email__ = "perceptron.origin@gmail.com"
+
+__license__ = "MIT License"
+__copyright__ = f"Copyright 2020 {__author__}"
