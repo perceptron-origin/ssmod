d4002049378057ec21cb1ecb8eb2ad7113df71f2
zhengp0 <zhengp@uw.edu>
2020-12-24 21:18:24 -0800

    add tests for the data utils and state

--- tests/test_state.py
+++ tests/test_state.py
@@ -2,7 +2,7 @@
 Test State Class
 """
 import pytest
-from numpy import shares_memory
+from numpy import shares_memory, isposinf, isneginf, allclose
 from numpy.random import seed, randn
 from ssmod.state import State
 
@@ -16,11 +16,45 @@ def x():
     return randn(3)
 
 
+@pytest.fixture
+def state(x):
+    return State(val=x)
+
+
 def test_state_ref(x):
     state = State(val=x)
     assert shares_memory(x, state.val)
 
 
-def test_state_dim(x):
-    state = State(val=x)
-    assert state.dim == x.size
+def test_state_default_bounds(state):
+    assert all(isneginf(state.bounds[0]))
+    assert all(isposinf(state.bounds[1]))
+
+
+def test_state_default_priors(state):
+    assert all(isposinf(state.prior[1]))
+    assert allclose(state.prior[0], 0.0)
+
+
+@pytest.mark.parametrize("lb", [0.0, [0.0, 0.0, 0.0]])
+@pytest.mark.parametrize("ub", [1.0, [1.0, 1.0, 1.0]])
+def test_set_bounds(state, lb, ub):
+    state.set_bounds(lb, ub)
+    assert allclose(state.bounds[0], 0.0)
+    assert allclose(state.bounds[1], 1.0)
+
+
+@pytest.mark.parametrize("mean", [0.0, [0.0, 0.0, 0.0]])
+@pytest.mark.parametrize("sd", [1.0, [1.0, 1.0, 1.0]])
+def test_set_prior(state, mean, sd):
+    state.set_prior(mean, sd)
+    assert allclose(state.prior[0], 0.0)
+    assert allclose(state.prior[1], 1.0)
+
+
+@pytest.mark.parametrize("mean", [0.0, [0.0, 0.0, 0.0]])
+@pytest.mark.parametrize("sd", [1.0, [1.0, 1.0, 1.0]])
+def test_set_posterior(state, mean, sd):
+    state.set_posterior(mean, sd)
+    assert allclose(state.posterior[0], 0.0)
+    assert allclose(state.posterior[1], 1.0)
